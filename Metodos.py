b0246ee390fe1cac7d07e533696152255f4c0bfd
selaresss <pabloselares@gmail.com>
2023-06-22 18:02:41 -0300

    enviando a segunda parte

--- Metodos.py
+++ Metodos.py
@@ -20,3 +20,18 @@ def listagemManifestacoesTipo(ocorrencias,conexao,tipo):
         print(len(ocorrencias))
         for ocorrencia in ocorrencias:
             print('codigo', str(ocorrencia[0]), '-', ocorrencia[1], '-', ocorrencia[2])
+def criarManifestacao(conexao,titulo,descricao,tipo,autor):
+    sqlInsercao = 'insert into ocorrencias (titulo,descricao,tipo,autor) values(%s,%s,%s,%s)'
+    valores = [titulo, descricao, tipo, autor]
+    insertNoBancoDados(conexao, sqlInsercao, valores)
+
+def quantidadePorTipoManifestacoes (conexao):
+    consultaListagem = 'select count(*) from ocorrencias'
+    resultado = listarBancoDados(conexao, consultaListagem)
+    quantidade = resultado[0][0]
+    print('Quantidade de Manifestações:', str(quantidade))
+
+    consultareclamacoes = "select count(*) from ocorrencias where tipo = 'reclamação'"
+    resultado = listarBancoDados(conexao, consultareclamacoes)
+    quantidadeReclamacoes = resultado[0][0]
+    print('Quantidade de reclamações:', str(quantidadeReclamacoes))
