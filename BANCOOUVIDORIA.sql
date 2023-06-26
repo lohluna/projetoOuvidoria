c5e3dd781ce08ccafda6f523cf977834f91841cd
Robertobr122 <rbbragaengenharia@gmail.com>
2023-06-26 17:04:40 -0300

    tabela sql

---
+++ BANCOOUVIDORIA.sql
@@ -0,0 +1,9 @@
+CREATE TABLE ocorrencias (
+   codigo int AUTO_INCREMENT,
+    titulo varchar (100),
+    descricao varchar (1000),
+    tipo varchar (100),
+    autor varchar (100),
+    primary key (codigo)
+);
+select * from ocorrencias
